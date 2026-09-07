9667d8ebe63bb56de8455a6fa60555f5c0bd16c7
elderdo <elderdo@yahoo.com>
2026-09-07 14:05:46 -0500

    Added Open Api packages and configured Program.cs to use Open API when running in dev.  Added a link to the Open API page from the default wwwroot/index.htm

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,11 @@
 using API.Data;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Add native OpenApi generation (replaces AddSwaggerGen)
+builder.Services.AddOpenApi(); 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
@@ -17,8 +19,27 @@ var app = builder.Build();
 app.UseCors(options => options.AllowAnyHeader()
   .AllowAnyMethod()
   .WithOrigins("http://localhost:4200","https://localhost:4200"));
+
+if (app.Environment.IsDevelopment())
+{
+  // 2. Map the raw OpenAPI JSON endpoint
+  app.MapOpenApi();
+  // 3. Map the Scalar API reference UI (replaces UseSwaggerUI)
+  // the endpoint is https://localhost:5001/scalar/v1 - (see ApplicationUrl for launchSettings.json)
+  app.MapScalarApiReference(options =>
+  {
+    options.Title = "DatingApp API Doc'";
+    options.Theme = ScalarTheme.Moon;
+    // Sets the default code-snippet Language in the UI panel
+    options.DefaultHttpClient = new(ScalarTarget.CSharp, ScalarClient.HttpClient);
+  });
+}
+
+// allow for the use of wwwroot and its index.html  
 app.UseDefaultFiles();
 app.UseStaticFiles(); // this serves index.html from wwwroot
+
+// Scan all the Controllers and use their WebApi
 app.MapControllers();
 
 app.Run();
