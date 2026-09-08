ac45fcca39ad73c2cb643ea4b4194607a2894234
elderdo <elderdo@yahoo.com>
2026-09-08 16:28:03 -0500

    indicated  jwt expiration is for dev and suggest for prod

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -30,7 +30,7 @@ public class TokenService(IConfiguration config) : ITokenService
     var tokenDescriptor = new SecurityTokenDescriptor
     {
       Subject = new ClaimsIdentity(claims),
-      Expires = DateTime.UtcNow.AddDays(7),
+      Expires = DateTime.UtcNow.AddDays(7), // for dev use this, for prod use a few minutes
       SigningCredentials = creds
     };
 
