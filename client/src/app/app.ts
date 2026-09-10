3d4c3fb6142ffa0183001758d7896047e56f46a7
elderdo <elderdo@yahoo.com>
2026-09-10 15:40:28 -0500

    Add navigation component and account service for user login functionality

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { Nav } from '../layout/nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -18,10 +19,10 @@ export class App implements OnInit {
   ngOnInit(): void {
     // Send a GET request to the DatingApp's endpoint:
     this.http.get<any[]>('https://localhost:5001/api/members').subscribe({
-        next: (response) => this.members.set(response),
-        error: (error) => console.log(error),
-        complete: () => console.log('Completed the http request'),
-        // when complete is finished, then we're unsubscribed from the response
-      });
+      next: (response) => this.members.set(response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Completed the http request'),
+      // when complete is finished, then we're unsubscribed from the response
+    });
   }
 }
