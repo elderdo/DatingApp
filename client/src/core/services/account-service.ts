1c522d06f15570262c39767b759d2eae45513bac
elderdo <elderdo@yahoo.com>
2026-09-11 12:44:21 -0500

    documented how service was created and where it is generated into.

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 
+// Created with cli: ng g s account-service
+// angular.json schematics configured to put services in "src/core/services"
 @Service()
 export class AccountService {
   private http = inject(HttpClient);
 
+  // C#'s .NET Web API server
   baseUrl = 'https://localhost:5001/api/';
 
   login(creds: any) {
+    // endpoint for the C# Web API's login POST
     return this.http.post(this.baseUrl + 'account/login', creds);
   }
 }
